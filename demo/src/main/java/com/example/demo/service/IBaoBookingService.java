d9bf60cc5ee1d54e533126dc06287755205500ec
baole1102 <levannhatbao29@gmail.com>
2024-01-05 09:02:02 +0700

    +up detail

--- demo/src/main/java/com/example/demo/service/IBaoBookingService.java
+++ demo/src/main/java/com/example/demo/service/IBaoBookingService.java
@@ -1,4 +1,15 @@
 package com.example.demo.service;
 
+import com.example.demo.dto.TourDTO;
+import com.example.demo.model.Img;
+import com.example.demo.model.Tour;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+
 public interface IBaoBookingService {
+    List<Tour> getAll();
+    Tour findById(int id);
+    List<Img> img(int id);
+
 }
