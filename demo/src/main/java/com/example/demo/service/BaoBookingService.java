91ae22216d64c72a653796e3a23f8b3d41b7b654
baole1102 <levannhatbao29@gmail.com>
2024-01-10 13:46:24 +0700

    push

--- demo/src/main/java/com/example/demo/service/BaoBookingService.java
+++ demo/src/main/java/com/example/demo/service/BaoBookingService.java
@@ -2,17 +2,17 @@ package com.example.demo.service;
 
 import com.example.demo.model.Booking;
 import com.example.demo.model.LocationTour;
+import com.example.demo.model.Payment;
 import com.example.demo.model.Tour;
 import com.example.demo.repository.IBaoBookingRepository;
+import com.example.demo.repository.IBaoPaymentRepository;
 import com.example.demo.repository.IBaoTourRepository;
 
 import com.example.demo.repository.IBaoLocationRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
-import java.sql.Date;
+import java.time.LocalDate;
 import java.util.List;
 
 @Service
@@ -22,7 +22,10 @@ public class BaoBookingService implements IBaoBookingService {
     @Autowired
     private IBaoLocationRepository baoLocationRepository;
     @Autowired
+    private IBaoPaymentRepository baoPaymentRepository;
+    @Autowired
     private IBaoBookingRepository baoBookingRepository;
+
     @Override
     public List<Tour> getAll() {
         return baoTourRepository.findAll();
@@ -54,8 +57,34 @@ public class BaoBookingService implements IBaoBookingService {
     }
 
     @Override
-    public List<Tour> searchSaveMoney(Date date) {
-        return baoTourRepository.searchSaveMoney(date);
+    public List<Tour> searchManyOption(LocalDate date) {
+        return baoTourRepository.searchManyOption(date);
     }
+
+    @Override
+    public List<Tour> searchHotTour(int careAbout) {
+        return baoTourRepository.searchHotTour(careAbout);
+    }
+
+    @Override
+    public List<Tour> searchSavePrice(int adultPrice) {
+        return baoTourRepository.searchSavePrice(adultPrice);
+    }
+
+    @Override
+    public void savePayment(Payment payment) {
+        baoPaymentRepository.save(payment);
+    }
+
+    @Override
+    public Payment findIdPayment(int id) {
+        return baoPaymentRepository.findIdPayment(id);
+    }
+
+    @Override
+    public Booking save(Booking booking) {
+        return baoBookingRepository.save(booking);
+    }
+
 
 }
