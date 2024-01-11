e333f8bb8686dd525f56b297d72bd551af2220f9
Quốc Giàu <quocgiau61s@gmail.com>
2024-01-11 08:20:54 +0700

    giau up

--- demo/src/main/java/com/example/demo/model/TourGuild.java
+++ demo/src/main/java/com/example/demo/model/TourGuild.java
@@ -1,12 +1,20 @@
 package com.example.demo.model;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.sql.Date;
 import java.util.Set;
 
 @Entity
 @Table(name = "tour_guild")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class TourGuild {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -19,73 +27,4 @@ public class TourGuild {
 
     @OneToMany(mappedBy = "tourGuild")
     private Set<Tour> tour;
-
-    public TourGuild() {
-    }
-
-    public TourGuild(int id, String name, Date date, String phoneGuild, String emailGuild, boolean isDelete, Set<Tour> tour) {
-        this.id = id;
-        this.name = name;
-        this.date = date;
-        this.phoneGuild = phoneGuild;
-        this.emailGuild = emailGuild;
-        this.isDelete = isDelete;
-        this.tour = tour;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public Date getDate() {
-        return date;
-    }
-
-    public void setDate(Date date) {
-        this.date = date;
-    }
-
-    public String getPhoneGuild() {
-        return phoneGuild;
-    }
-
-    public void setPhoneGuild(String phoneGuild) {
-        this.phoneGuild = phoneGuild;
-    }
-
-    public String getEmailGuild() {
-        return emailGuild;
-    }
-
-    public void setEmailGuild(String emailGuild) {
-        this.emailGuild = emailGuild;
-    }
-
-    public boolean isDelete() {
-        return isDelete;
-    }
-
-    public void setDelete(boolean delete) {
-        isDelete = delete;
-    }
-
-    public Set<Tour> getTour() {
-        return tour;
-    }
-
-    public void setTour(Set<Tour> tour) {
-        this.tour = tour;
-    }
 }
