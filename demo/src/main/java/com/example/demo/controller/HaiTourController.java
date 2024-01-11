e8835aa99b9a68b0dd3d7d748452d812757ef34b
haitrongnguyen <143687541+haitrongnguyen@users.noreply.github.com>
2024-01-11 16:46:07 +0700

    +uptour

--- demo/src/main/java/com/example/demo/controller/HaiTourController.java
+++ demo/src/main/java/com/example/demo/controller/HaiTourController.java
@@ -17,6 +17,8 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.util.FileCopyUtils;
@@ -34,6 +36,8 @@ import java.util.Optional;
 @Controller
 @RequestMapping("/tours")
 public class HaiTourController {
+    @Autowired
+    JavaMailSender javaMailSender;
 
     @Autowired
     private IHaiTourService haiTourService;
@@ -100,19 +104,38 @@ public class HaiTourController {
             return "/hai_tour/edit";
         }
         for (Tour t : haiTourService.findAll()) {
-            if ((tourDTO.getDepartureDate().isAfter(t.getDepartureDate()) && tourDTO.getDepartureDate().isBefore(t.getEndDate().plusDays(1)))
-                    || (tourDTO.getEndDate().isAfter(t.getDepartureDate().minusDays(1)) && tourDTO.getEndDate().isBefore(t.getEndDate()))) {
-                if (t.getTourGuild() == tourDTO.getTourGuild()) {
-                    model.addAttribute("mess", "Hướng dẫn viên " +t.getTourGuild().getName()+" có lịch trong thời gian "+t.getDepartureDate().format(DateTimeFormatter.ofPattern("dd-MM-yyyy")) +" đến "+t.getEndDate().format(DateTimeFormatter.ofPattern("dd-MM-yyyy"))+ " ! Vui lòng chọn HDV khác");
-                    return "/hai_tour/edit";
+                if ((tourDTO.getDepartureDate().isAfter(t.getDepartureDate()) && tourDTO.getDepartureDate().isBefore(t.getEndDate().plusDays(1)))
+                        || (tourDTO.getEndDate().isAfter(t.getDepartureDate().minusDays(1)) && tourDTO.getEndDate().isBefore(t.getEndDate()))) {
+                    if (t.getTourGuild() == tourDTO.getTourGuild() && t.getId()!= tourDTO.getId()) {
+                        model.addAttribute("mess", "Hướng dẫn viên " + t.getTourGuild().getName() + " có lịch trong thời gian " + t.getDepartureDate().format(DateTimeFormatter.ofPattern("dd-MM-yyyy")) + " đến " + t.getEndDate().format(DateTimeFormatter.ofPattern("dd-MM-yyyy")) + " ! Vui lòng chọn HDV khác");
+                        return "/hai_tour/edit";
+                    }
                 }
-            }
 
         }
         Optional<Tour> tour = haiTourService.findById(id);
-        BeanUtils.copyProperties(tourDTO, tour);
+        BeanUtils.copyProperties(tourDTO, tour.get());
         haiTourService.save(tour.get());
-        redirectAttributes.addFlashAttribute("message", "Sửa thành công");
+        TourGuild tourGuild = tour.get().getTourGuild();
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setTo(tourGuild.getEmailGuild());
+        List<LocationTour> locationTours = haiLocationTourService.findAllByTour_Id(tour.get().getId());
+        message.setSubject("Bạn có một tour mới");
+        StringBuilder content = new StringBuilder("Dear, " + tourGuild.getName() + "\n" +
+                "Ngày " + tour.get().getDepartureDate() + " đến ngày " + tour.get().getEndDate() + " bạn sẽ dẫn một tour mới\n" +
+                "Thông tin tour:  \n" +
+                "Số lượng người: " + tour.get().getQuantity() + "\n" +
+                "Chi tiết: " + tour.get().getDescription() + "\n" +
+                "Thời gian khởi hành : 8h ngày " + tour.get().getDepartureDate() + "\n" +
+                "Lịch trình: \n");
+        for(LocationTour l: locationTours){
+            content.append("Ngày ").append(l.getTourDay()).append(" ").append(l.getLocation()).append("\n");
+            content.append(l.getDescriptionLocation()).append("\n");
+        }
+        content.append("Vui lòng xác nhận với nhân sự để nhận tour hoặc ghi nhận thay đổi! \n" + "Xin cảm ơn!");
+        message.setText(content.toString());
+        javaMailSender.send(message);
+        redirectAttributes.addFlashAttribute("message", 1);
         return "redirect:/tours/"+tour.get().getId()+"/detail";
 
     }
@@ -120,6 +143,8 @@ public class HaiTourController {
     @GetMapping("/create")
     public String createTour(Model model) {
         model.addAttribute("tour", new TourDTO());
+        List<LocationTour> locationTours = haiLocationTourService.findAll();
+        model.addAttribute("locationTour",locationTours);
         return "/hai_tour/edit";
     }
 
@@ -149,6 +174,7 @@ public class HaiTourController {
         Tour tour = new Tour();
         BeanUtils.copyProperties(tourDTO, tour);
         haiTourService.save(tour);
+        TourGuild tourGuild = tour.getTourGuild();
         model.addAttribute("tour",tour);
         model.addAttribute("df", DateTimeFormatter.ofPattern("dd-MM-yyyy"));
         return "redirect:/tours/"+tour.getId()+"/detail";
@@ -178,13 +204,16 @@ public class HaiTourController {
 
     @PostMapping("/save-location")
     public String saveLocation(@Valid @ModelAttribute("location") LocationTourDTO locationTourDTO,
-                               BindingResult bindingResult,Model model ){
-
+                               BindingResult bindingResult,Model model,
+                               RedirectAttributes redirectAttributes){
+        new LocationTourDTO().validate(locationTourDTO,bindingResult);
         if (bindingResult.hasFieldErrors()) {
             Optional<Tour> tour = haiTourService.findById(locationTourDTO.getTour().getId());
             model.addAttribute("tour",tour.get());
             model.addAttribute("df", DateTimeFormatter.ofPattern("dd-MM-yyyy"));
             model.addAttribute("location", locationTourDTO);
+            List<LocationTour> locationTours = haiLocationTourService.findAll();
+            model.addAttribute("locationTour",locationTours);
             model.addAttribute("messVaild","Chưa thêm được điểm đến");
             return "/hai_tour/detail";
         }
@@ -197,7 +226,9 @@ public class HaiTourController {
         model.addAttribute("tour",tour.get());
         List<LocationTour> locationTours = haiLocationTourService.findAll();
         model.addAttribute("locationTour",locationTours);
+        redirectAttributes.addFlashAttribute("message",1);
         return "redirect:/tours/"+tour.get().getId()+"/detail";
+//        return "hai_tour/detail";
     }
     @PostMapping("/edit-location")
     public String showForm(@RequestParam("id-edit") int id,
@@ -207,6 +238,8 @@ public class HaiTourController {
         model.addAttribute("location",locationTour.get());
         model.addAttribute("tour",tour.get());
         model.addAttribute("df", DateTimeFormatter.ofPattern("dd-MM-yyyy"));
+        List<LocationTour> locationTours = haiLocationTourService.findAll();
+        model.addAttribute("locationTour",locationTours);
         return "/hai_tour/detail";
     }
     @PostMapping("/delete-location")
@@ -223,7 +256,33 @@ public class HaiTourController {
         List<LocationTour> locationTours = haiLocationTourService.findAll();
         model.addAttribute("locationTour",locationTours);
         model.addAttribute("location",locationTourDTO);
-        redirectAttributes.addFlashAttribute("msg",1);
+        redirectAttributes.addFlashAttribute("message",2);
         return "redirect:/tours/"+tour.get().getId()+"/detail";
+    }
+    @GetMapping("/sendEmail/{id}")
+    public String sendMail(@PathVariable("id") int id,
+                           RedirectAttributes redirectAttributes){
+        Tour tour = haiTourService.findById(id).get();
+        TourGuild tourGuild = tour.getTourGuild();
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setTo(tourGuild.getEmailGuild());
+        List<LocationTour> locationTours = haiLocationTourService.findAllByTour_Id(tour.getId());
+        message.setSubject("Bạn có một tour mới");
+        StringBuilder content = new StringBuilder("Dear, " + tourGuild.getName() + "\n" +
+                "Ngày " + tour.getDepartureDate() + " đến ngày " + tour.getEndDate() + " bạn sẽ dẫn một tour mới\n" +
+                "Thông tin tour:  \n" +
+                "Số lượng người: " + tour.getQuantity() + "\n" +
+                "Chi tiết: " + tour.getDescription() + "\n" +
+                "Thời gian khởi hành : 8h ngày " + tour.getDepartureDate() + "\n" +
+                "Lịch trình: \n");
+                for(LocationTour l: locationTours){
+                    content.append("Ngày ").append(l.getTourDay()).append(" ").append(l.getLocation()).append("\n");
+                    content.append(l.getDescriptionLocation()).append("\n");
+                }
+                content.append("Vui lòng xác nhận với nhân sự để nhận tour hoặc ghi nhận thay đổi! \n" + "Xin cảm ơn!");
+        message.setText(content.toString());
+        javaMailSender.send(message);
+        redirectAttributes.addFlashAttribute("message",3);
+        return "redirect:/tours/"+id+"/detail";
     }
 }
